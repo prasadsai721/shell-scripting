bf2049ca46ea3b857e9788b5bd6bfe479b8676fb
prasa <prasadsai721@gmail.com>
2022-03-16 01:23:11 +1100

    Makking frontend component

--- shell-scripting/roboshop/components/frontend.sh
+++ shell-scripting/roboshop/components/frontend.sh
@@ -1,1 +1,22 @@
-echo frontend component
+#!/bin/bash
+
+
+
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+#rm -rf *
+#unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+systemctl start nginx
+
