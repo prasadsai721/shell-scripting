80636af83359d1d881a0fff19fe042de53f89c64
prasa <prasadsai721@gmail.com>
2022-03-16 16:35:24 +1100

    condition script

--- shell-scripting/roboshop/components/frontend.sh
+++ shell-scripting/roboshop/components/frontend.sh
@@ -8,9 +8,23 @@ if [ "$USER_ID" -ne 0 ]; then
 
 echo -e"\e[36m installing nginx \e[0m"
 yum install nginx -y
+if[ $? -eq 0 ]; then
+ echo "\e[32msuccess\e[0m]"
+else
+ echo "\e[31mFAILURE\e[0m]"
+exit 1
+fi
 
+echo -e"\e[36m Downloading nginx \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if[ $? -eq 0 ]; then
+ echo "\e[32msuccess\e[0m]"
+else
+ echo "\e[31mFAILURE\e[0m]"
+exit 1
+fi
 
+echo -e"\e[36m cleaningup old nginx contant and extract new download archive\e[0m"
 cd /usr/share/nginx/html
 rm -rf
 unzip /tmp/frontend.zip
@@ -18,9 +32,14 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if[ $? -eq 0 ]; then
+ echo "\e[32msuccess\e[0m]"
+else
+ echo "\e[31mFAILURE\e[0m]"
+exit 1
+fi
 
 echo -e"\e[36m starting nginx \e[0m"
-
 systemctl enable nginx
 systemctl restart nginx
 
