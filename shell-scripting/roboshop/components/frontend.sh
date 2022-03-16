f0301777c354d9273984ce6e55ab4f59712dd5d8
prasa <prasadsai721@gmail.com>
2022-03-16 16:44:33 +1100

    condition script

--- shell-scripting/roboshop/components/frontend.sh
+++ shell-scripting/roboshop/components/frontend.sh
@@ -18,9 +18,9 @@ fi
 echo -e"\e[36m Downloading nginx \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 if[ $? -eq 0 ]; then
- echo "\e[32msuccess\e[0m"
+ echo -e "\e[32msuccess\e[0m"
 else
- echo "\e[31mFAILURE\e[0m"
+ echo -e "\e[31mFAILURE\e[0m"
 exit 2
 fi
 
@@ -33,13 +33,13 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 if[ $? -eq 0 ]; then
- echo "\e[32msuccess\e[0m"
+ echo -e "\e[32msuccess\e[0m"
 else
- echo "\e[31mFAILURE\e[0m"
+ echo -e "\e[31mFAILURE\e[0m"
 exit 2
 fi
 
-echo -e"\e[36m starting nginx \e[0m"
+echo -e "\e[36m starting nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
 
