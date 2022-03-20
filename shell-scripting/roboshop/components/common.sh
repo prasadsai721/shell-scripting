13ad94709a47138bcabcd05e9480c34b8f79d605
prasa <prasadsai721@gmail.com>
2022-03-20 16:23:28 +1100

    common

--- shell-scripting/roboshop/components/common.sh
+++ shell-scripting/roboshop/components/common.sh
@@ -49,14 +49,7 @@ SERVICE_SETUP() {
 
   Print "Setup SystemD File"
   sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
-          -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
-          -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
-          -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
-          -e 's/CARTENDPOINT/cart.roboshop.internal/' \
-          -e 's/DBHOST/mysql.roboshop.internal/' \
-          -e 's/CARTHOST/cart.roboshop.internal/' \
-          -e 's/USERHOST/user.roboshop.internal/' \
-          -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
+
           /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG_FILE}
   StatCheck $?
 
